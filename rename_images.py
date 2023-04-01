757197b1b8a44f975be48a7293b353424512294b
Edgardo610 <Edgardo610@users.noreply.github.com>
2023-04-01 14:06:03 -0400

    script pretraitement, data dragibus et reglisse

---
+++ rename_images.py
@@ -0,0 +1,20 @@
+import os
+
+# Set the directory path where the images are located
+directory_path = './raw_data/dragibus'
+
+# Get a list of all files in the directory
+file_list = os.listdir(directory_path)
+
+# Filter the list to only include JPG images
+jpg_list = [f for f in file_list if f.endswith('.jpg')]
+
+# Sort the JPG list alphabetically
+jpg_list.sort()
+
+# Loop through the JPG list and rename each file with an index number
+for i, filename in enumerate(jpg_list):
+    new_filename = str(i) + '.jpg'
+    old_path = os.path.join(directory_path, filename)
+    new_path = os.path.join(directory_path, new_filename)
+    os.rename(old_path, new_path)
---
+++ resize_images.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import os
+
+# Set the directory path where the images are located
+directory_path = './raw_data/rotella'
+
+# Get a list of all files in the directory
+file_list = os.listdir(directory_path)
+
+# Filter the list to only include images
+image_list = [f for f in file_list if f.endswith('.jpg') or f.endswith('.png')]
+
+# Loop through the image list and resize each image
+for filename in image_list:
+    file_path = os.path.join(directory_path, filename)
+    with Image.open(file_path) as img:
+        # Resize the image to 128x128 pixels
+        img_resized = img.resize((128, 128))
+        
+        # Save the resized image with the suffix '_128'
+        new_filename = os.path.splitext(filename)[0] + '_128' + os.path.splitext(filename)[1]
+        new_path = os.path.join(directory_path, new_filename)
+        img_resized.save(new_path)
